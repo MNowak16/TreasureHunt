90d01ffbb3cd8f9b3c185921fed7267756f46b18
mnowak16 <mnowak16@my.wctc.edu>
2022-03-30 21:17:52 -0500

    initial load

---
+++ code.js
@@ -0,0 +1,54 @@
+$("area#t-rex").on("click", function() {
+    foundTRex();
+    $(lTRex).css("text-decoration", "line-through");
+});
+$("area#pterodactyl").on("click", function() {
+    foundPterodactyl();
+    $(lPtero).css("text-decoration", "line-through");
+});
+$("area#brachiosaurus").on("click", function() {
+    foundBrachiosaurus();
+    $(lBrachio).css("text-decoration", "line-through");
+});
+$("area#small_stegosaurus").on("click", function() {
+    foundSmallStegosaurus();
+    $(lSmStego).css("text-decoration", "line-through");
+});
+$("area#large_stegosaurus").on("click", function() {
+    foundLargeStegosaurus();
+    $(lLgStego).css("text-decoration", "line-through");
+});
+$("area#seahorse").on("click", function() {
+    foundSeahorse();
+    $(lSeahorse).css("text-decoration", "line-through");
+});
+$("area#infinity").on("click", function() {
+    foundInfinity();
+    $(lInfinity).css("text-decoration", "line-through");
+});
+
+function fountIt(description) {
+    $("p#foundItem").append(`You found the ${description}!<br>`);
+}
+
+function foundTRex() {
+    fountIt("T-Rex")
+}
+function foundPterodactyl() {
+    fountIt("Pterodactyl")
+}
+function foundBrachiosaurus() {
+    fountIt("Brachiosaurus")
+}
+function foundSmallStegosaurus() {
+    fountIt("Small Stegosaurus")
+}
+function foundLargeStegosaurus() {
+    fountIt("Large Stegosaurus")
+}
+function foundSeahorse() {
+    fountIt("Seahorse")
+}
+function foundInfinity() {
+    fountIt("Infinity")
+}
